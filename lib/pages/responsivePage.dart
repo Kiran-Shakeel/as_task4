beb3b0cb4ff2c64cc258701a864a6c08b44b258f
Kiran-Shakeel <kiranshakeel02@gmail.com>
2021-08-05 03:25:12 +0500

    add scroll view

--- lib/pages/responsivePage.dart
+++ lib/pages/responsivePage.dart
@@ -12,65 +12,67 @@ class ResponsivePage extends StatelessWidget {
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Result(
-                widthSize: size.width,
-                heightSize: size.height * 0.2,
-                color: Colors.purple,
-                content: "Column1",
-              )
-            ],
-          ),
-          Column(
-            children: [
-              Row(
-                children: [
-                  Result(
-                    widthSize: size.width * 0.5,
-                    heightSize: size.height * 0.2,
-                    color: Colors.lightGreen,
-                    content: "Column 2,Row 1",
-                  ),
-                  Result(
-                    widthSize: size.width * 0.5,
-                    heightSize: size.height * 0.2,
-                    color: Colors.orange,
-                    content: "Column 2,Row 2",
-                  ),
-                ],
-              )
-            ],
-          ),
-          Column(
-            children: [
-              Row(
-                children: [
-                  Result(
-                    widthSize: size.width * 0.5,
-                    heightSize: size.height * 0.2,
-                    color: Colors.red,
-                    content: "Column 3,Row 1",
-                  ),
-                  Result(
-                    widthSize: size.width * 0.5,
-                    heightSize: size.height * 0.2,
-                    color: Colors.blue,
-                    content: "Column 3,Row 2",
-                  ),
-                ],
-              )
-            ],
-          ),
-          Result(
-            widthSize: size.width,
-            heightSize: size.height * 0.2,
-            color: Colors.yellow,
-            content: "Column 4",
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Result(
+                  widthSize: size.width,
+                  heightSize: size.height * 0.2,
+                  color: Colors.purple,
+                  content: "Column1",
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Result(
+                      widthSize: size.width * 0.5,
+                      heightSize: size.height * 0.2,
+                      color: Colors.lightGreen,
+                      content: "Column 2,Row 1",
+                    ),
+                    Result(
+                      widthSize: size.width * 0.5,
+                      heightSize: size.height * 0.2,
+                      color: Colors.orange,
+                      content: "Column 2,Row 2",
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Result(
+                      widthSize: size.width * 0.5,
+                      heightSize: size.height * 0.2,
+                      color: Colors.red,
+                      content: "Column 3,Row 1",
+                    ),
+                    Result(
+                      widthSize: size.width * 0.5,
+                      heightSize: size.height * 0.2,
+                      color: Colors.blue,
+                      content: "Column 3,Row 2",
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Result(
+              widthSize: size.width,
+              heightSize: size.height * 0.2,
+              color: Colors.yellow,
+              content: "Column 4",
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -97,7 +99,7 @@ class Result extends StatelessWidget {
       child: Center(
           child: Text(
         content,
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
       )),
     );
   }
